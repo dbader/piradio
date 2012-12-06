17266170d514cf5874601d8b93f4a94ed2186ccf
Daniel Bader <mail@dbader.org>
2012-12-06 20:41:34 +0100

    Add backlight and contrast settings to lcd driver.

--- lcd.py
+++ lcd.py
@@ -1,25 +1,30 @@
 import ctypes
 import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 LCD_WIDTH, LCD_HEIGHT = 128, 64
-raspilcd = ctypes.cdll.LoadLibrary("./libraspilcd.so")
-buttons = ctypes.c_uint8.in_dll(raspilcd, "Button")
-
 KEY_CENTER = 0x04
 KEY_LEFT = 0x02
 KEY_RIGHT = 0x08
 KEY_UP = 0x01
 KEY_DOWN = 0x10
+
+raspilcd = ctypes.cdll.LoadLibrary("./libraspilcd.so")
+buttons = ctypes.c_uint8.in_dll(raspilcd, "Button")
 _KEYS = [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, KEY_CENTER]
 
-def init():
+def init(debug=True):
     raspilcd.RaspiLcdHwInit()
     raspilcd.LCD_Init()
     raspilcd.LCD_ClearScreen()
     raspilcd.LCD_WriteFramebuffer()
-    raspilcd.LCD_SetPenColor(1)
-    raspilcd.LCD_DrawLine(0, 0, LCD_WIDTH-1, LCD_HEIGHT-1)
-    raspilcd.LCD_WriteFramebuffer()
+    if debug:
+        raspilcd.LCD_SetPenColor(1)
+        raspilcd.LCD_DrawLine(0, 0, LCD_WIDTH-1, LCD_HEIGHT-1)
+        raspilcd.LCD_DrawLine(0, LCD_HEIGHT-1, LCD_WIDTH-1, 0)
+        raspilcd.LCD_WriteFramebuffer()
 
 def readkeys():
     raspilcd.UpdateButtons()
@@ -36,14 +41,25 @@ def update(pixels):
                 raspilcd.LCD_PutPixel(x, y)
     raspilcd.LCD_WriteFramebuffer()
 
-#raspilcd.SetBacklight(32)
-#raspilcd.LCD_SetContrast(18)
+def set_contrast(c):
+    MAX_CONTRAST = 10
+    value = int(min(MAX_CONTRAST, max(0, c)) * MAX_CONTRAST)
+    logging.debug('Setting contrast to %.2f (%i)', c, value)
+    raspilcd.LCD_SetContrast(value)
+
+def set_backlight_enabled(enabled):
+    logging.debug('Setting backlight to %s', 'on' if enabled else 'off')
+    raspilcd.SetBacklight(enabled)
 
 if __name__ == '__main__':
     init()
-    t = 1.0 / 8.0
+    set_contrast(0.6)
+    set_backlight_enabled(True)
+    t = 1.0
     while not True in readkeys():
         update([0] * (LCD_WIDTH * LCD_HEIGHT))
+        set_backlight_enabled(False)
         time.sleep(t)
         update([1] * (LCD_WIDTH * LCD_HEIGHT))
-	time.sleep(t)
+        set_backlight_enabled(True)
+        time.sleep(t)
